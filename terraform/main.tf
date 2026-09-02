83b82e0eb75e8468ab18592f45d6f67b8865c0cf
sujal-hp <suranisujal19@gmail.com>
2026-09-02 13:21:09 +0530

    add main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "k3s_server" {
+  ami="ami-01a00762f46d584a1"
+  instance_type = "t3.micro"
+  
+  vpc_security_group_ids = [ aws_security_group.k3s_sg.id ]
+  iam_instance_profile = aws_iam_instance_profile.ssm_profile.name
+
+  tags = {
+    Name = "AWS-K3s_Host"
+  }
+}
