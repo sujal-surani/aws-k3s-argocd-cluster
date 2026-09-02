485dea6168f6d2ee8b9f0c52967c5ea0fab62861
sujal-hp <suranisujal19@gmail.com>
2026-09-02 13:21:33 +0530

    add security.tf

---
+++ terraform/security.tf
@@ -0,0 +1,32 @@
+resource "aws_security_group" "k3s_sg" {
+    name = "aws-k3s-sg"
+    description = "Security group for k3s GitOps cluster"
+
+    ingress {
+        from_port = 6433
+        to_port = 6433
+        protocol = "tcp"
+        cidr_blocks = [ "0.0.0.0/0" ]
+    }
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = [ "0.0.0.0/0" ]
+    }
+
+    ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = [ "0.0.0.0/0" ]
+    }
+
+    egress{
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = [ "0.0.0.0/0" ]
+    }
+}
