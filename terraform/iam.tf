5760f66e9d04b5f3d1d0b78ce6f6e50d971b2e82
sujal-hp <suranisujal19@gmail.com>
2026-09-02 13:28:56 +0530

    add iam.tf

---
+++ terraform/iam.tf
@@ -0,0 +1,21 @@
+resource "aws_iam_role" "ssm_role" {
+  name = "k2s-ssm-role"
+  assume_role_policy = jsondecode({
+    Version = "2012-10-17",
+    Statement = [{
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = { Service = "ec2.amazonaws.com" }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_policy" {
+  role = aws_iam_role.ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSGManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "ssm_profile" {
+  name = "k3s-ssm-profile"
+  role = aws_iam_role.ssm_role.name
+}
